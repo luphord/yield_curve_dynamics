20ddaa2043a3bd0aec11247b61a679e0f3e0d186
luphord <luphord@protonmail.com>
2019-03-18 17:13:05 +0100

    extracted pca

--- yield_curve_dynamics/pca.py
+++ yield_curve_dynamics/pca.py
@@ -1,5 +1,12 @@
 import numpy as np
+from sklearn.decomposition import PCA
 
 
 def shifted_log_diff(rates, shift=2.0):
     return np.log(rates + shift).diff(axis=0).iloc[1:]
+
+
+def perform_pca(rate_changes):
+    pca = PCA()
+    pca.fit(rate_changes)
+    return pca
