53a2815e4049f59f3f45dee9c0e97fce077c2467
luphord <luphord@protonmail.com>
2019-03-19 06:51:21 +0100

    extracted pca plotting functions

--- yield_curve_dynamics/pca.py
+++ yield_curve_dynamics/pca.py
@@ -1,5 +1,6 @@
 import numpy as np
 from sklearn.decomposition import PCA
+import matplotlib.pyplot as plt
 
 
 def shifted_log_diff(rates, shift=2.0):
@@ -10,3 +11,24 @@ def perform_pca(rate_changes):
     pca = PCA()
     pca.fit(rate_changes)
     return pca
+
+
+def plot_pca1(pca):
+    fig, ax = plt.subplots(figsize=(12, 8))
+    normalized_sv = pca.singular_values_ / pca.singular_values_.sum()
+    ax.bar(range(len(normalized_sv)), normalized_sv)
+    ax.plot(np.cumsum(normalized_sv), 'r')
+    return fig, ax
+
+
+def plot_pca2(pca):
+    fig, ax = plt.subplots(figsize=(12, 8))
+    normalized_sv = pca.explained_variance_ratio_
+    ax.bar(range(len(normalized_sv)), normalized_sv)
+    ax.plot(np.cumsum(normalized_sv), 'r')
+    return fig, ax
+
+
+def plot_pca_components(pca):
+    plt.plot(pca.components_[:3, :].T)
+    plt.legend(['first', 'second', 'third'])
