b05183341c4785008ed4aaf9f13c824427417f35
luphord <luphord@protonmail.com>
2019-03-20 06:44:26 +0100

    transform data from ECB

--- yield_curve_dynamics/cli.py
+++ yield_curve_dynamics/cli.py
@@ -3,7 +3,7 @@
 '''Main command line interface for Yield Curve Dynamics.'''
 import click
 
-from .data import cli_load_csv
+from .data import cli_load_csv, cli_transform
 from .video import cli_video
 
 
@@ -14,4 +14,5 @@ def main():
 
 
 main.add_command(cli_load_csv)
+main.add_command(cli_transform)
 main.add_command(cli_video)
--- yield_curve_dynamics/data.py
+++ yield_curve_dynamics/data.py
@@ -1,5 +1,11 @@
+import io
+import csv
+import zipfile
+from collections import defaultdict
+
 import click
 import pandas as pd
+
 from nelson_siegel_svensson import NelsonSiegelSvenssonCurve
 
 
@@ -19,3 +25,45 @@ def cli_load_csv(csv_file):
     curves = list(load_csv(csv_file))
     click.echo('Successfully loaded {} curves'.format(len(curves)))
     return 0
+
+
+_relevant_params = ['BETA0', 'BETA1', 'BETA2', 'BETA3', 'TAU1', 'TAU2']
+
+
+@click.command(name='transform')
+@click.option('-i', '--ecb-input',
+              type=click.Path(exists=True, file_okay=True, dir_okay=False,
+                              resolve_path=True),
+              required=True,
+              help='ZIP file containing a CSV as provided ' +
+                   'by European Central Bank')
+@click.option('-o', '--csv-output',
+              type=click.Path(exists=False, file_okay=True, dir_okay=False,
+                              resolve_path=True),
+              required=True,
+              help='CSV output file with Nelson-Siegel-Svensson parameters')
+def cli_transform(ecb_input, csv_output):
+    '''Load data as provided by ECB and transform to parameters CSV.'''
+    click.echo('Loading ECB data from {}'.format(ecb_input))
+    with zipfile.ZipFile(ecb_input) as z:
+        with io.TextIOWrapper(z.open('data.csv', 'r')) as f:
+            reader = csv.DictReader(f)
+            filtered = [dict(param=row['DATA_TYPE_FM'],
+                             date=row['TIME_PERIOD'],
+                             value=row['OBS_VALUE'])
+                        for row in reader
+                        if row['DATA_TYPE_FM'] in _relevant_params]
+
+    pivot = defaultdict(dict)
+    for row in filtered:
+            d = pivot[row['date']]
+            d['date'] = row['date']
+            d[row['param']] = row['value']
+
+    pivot = sorted(pivot.values(), key=lambda row: row['date'])
+    headers = ['date'] + _relevant_params
+    click.echo(f'Writing Nelson-Siegel-Svensson parameters to {csv_output}')
+    with open(csv_output, 'w') as f:
+        writer = csv.DictWriter(f, headers)
+        writer.writeheader()
+        writer.writerows(pivot)
