8fd4624083fbf0fc311e1df64dd32a955f2ca354
luphord <luphord@protonmail.com>
2019-04-01 06:59:58 +0200

    store as zip file

--- yield_curve_dynamics/download.py
+++ yield_curve_dynamics/download.py
@@ -1,3 +1,5 @@
+import zipfile
+
 import click
 
 
@@ -11,7 +13,7 @@ def download(start_date, end_date, output_file):
 @click.option('-e', '--end-date', type=str, required=True,
               help='end date for curve download')
 @click.option('-f', '--zip-file',
-              type=click.Path(exists=True, file_okay=True, dir_okay=False,
+              type=click.Path(file_okay=True, dir_okay=False,
                               resolve_path=True),
               required=True,
               help='ZIP file for download output')
@@ -19,5 +21,7 @@ def cli_download(start_date, end_date, zip_file):
     '''Download raw yield curve data parameters from ECB.'''
     click.echo('Downloading ECB yield curve data ' +
                f'from {start_date} until {end_date} and storing as {zip_file}')
-    download(start_date, end_date, zip_file)
+    with zipfile.ZipFile(zip_file, 'w') as z:
+        with z.open('data.csv', 'w') as csv_file:
+            download(start_date, end_date, csv_file)
     return 0
