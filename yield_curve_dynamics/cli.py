1588482611ab50b230afe3d2d0347be8c94b966f
luphord <luphord@protonmail.com>
2019-04-01 06:48:28 +0200

    wrapper for download

--- yield_curve_dynamics/cli.py
+++ yield_curve_dynamics/cli.py
@@ -3,6 +3,7 @@
 '''Main command line interface for Yield Curve Dynamics.'''
 import click
 
+from .download import cli_download
 from .data import cli_load_csv, cli_transform
 from .video import cli_video
 
@@ -13,6 +14,7 @@ def main():
     pass
 
 
+main.add_command(cli_download)
 main.add_command(cli_load_csv)
 main.add_command(cli_transform)
 main.add_command(cli_video)
---
+++ yield_curve_dynamics/download.py
@@ -0,0 +1,23 @@
+import click
+
+
+def download(start_date, end_date, output_file):
+    pass
+
+
+@click.command(name='download')
+@click.option('-s', '--start-date', type=str, required=True,
+              help='start date for curve download')
+@click.option('-e', '--end-date', type=str, required=True,
+              help='end date for curve download')
+@click.option('-f', '--zip-file',
+              type=click.Path(exists=True, file_okay=True, dir_okay=False,
+                              resolve_path=True),
+              required=True,
+              help='ZIP file for download output')
+def cli_download(start_date, end_date, zip_file):
+    '''Download raw yield curve data parameters from ECB.'''
+    click.echo('Downloading ECB yield curve data ' +
+               f'from {start_date} until {end_date} and storing as {zip_file}')
+    download(start_date, end_date, zip_file)
+    return 0
